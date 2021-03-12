ccd7a0fa93e0e5167baee018f8d49e21e6234ba7
DESKTOP-9MUFES4\K-B <k-b0514@hotmail.com>
2021-03-12 11:17:29 -0800

    06 -  Introducing Coroutines

--- RealmRush/Assets/Enemy/EnemyMover.cs
+++ RealmRush/Assets/Enemy/EnemyMover.cs
@@ -5,18 +5,20 @@ using UnityEngine;
 public class EnemyMover : MonoBehaviour
 {
     [SerializeField] List<Waypoint> path = new List<Waypoint>();
+    [SerializeField] float timer = 1f;
 
     // Start is called before the first frame update
     void Start()
     {
-        PrintWaypointName();
+       StartCoroutine(PrintWaypointName());
     }
 
-    void PrintWaypointName()
+    IEnumerator PrintWaypointName()
     {
         foreach(Waypoint waypoint in path)
         {
-            Debug.Log(waypoint.name);
+            this.transform.position = waypoint.transform.position;
+            yield return new WaitForSeconds(timer);
         }
     }
 }
