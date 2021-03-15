0d8e2ea0537504512b8382cd3ce4b262e47b32d3
DESKTOP-9MUFES4\K-B <k-b0514@hotmail.com>
2021-03-15 05:48:50 -0700

    10 - Detect Mouse Input

--- RealmRush/Assets/Enemy/Waypoint.cs
+++ RealmRush/Assets/Enemy/Waypoint.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class Waypoint : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] bool IsPlaceable;
+    private void OnMouseOver()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (IsPlaceable && Input.GetMouseButtonDown(0))
+        {
+            Debug.Log(transform.name);
+        }
     }
 }
