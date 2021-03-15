cd84d4fca1eb15782f34696ff35ed452a31ba309
DESKTOP-9MUFES4\K-B <k-b0514@hotmail.com>
2021-03-15 04:24:04 -0700

    09 - Smooth Enemy Movement

--- RealmRush/Assets/Enemy/EnemyMover.cs
+++ RealmRush/Assets/Enemy/EnemyMover.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class EnemyMover : MonoBehaviour
 {
     [SerializeField] List<Waypoint> path = new List<Waypoint>();
-    [SerializeField] float timer = 1f;
+    [SerializeField] [Range(0.0f, 5.0f)] float speed = 1f;
 
     // Start is called before the first frame update
     void Start()
@@ -17,8 +17,18 @@ public class EnemyMover : MonoBehaviour
     {
         foreach(Waypoint waypoint in path)
         {
-            this.transform.position = waypoint.transform.position;
-            yield return new WaitForSeconds(timer);
+            Vector3 startPosition = this.transform.position;
+            Vector3 endPosition = waypoint.transform.position;
+            float travelPercent = 0.0f;
+
+            transform.LookAt(endPosition);
+
+            while(travelPercent < 1.0f)
+            {
+                travelPercent += Time.deltaTime * speed;
+                transform.position = Vector3.Lerp(startPosition, endPosition, travelPercent);
+                yield return new WaitForEndOfFrame();
+            }
         }
     }
 }
